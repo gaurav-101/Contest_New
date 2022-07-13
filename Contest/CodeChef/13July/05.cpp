ac03478bd6d91aff68d99dee50e6cfb123805bc9
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-13 23:08:58 +0530

    init

---
+++ Contest/CodeChef/13July/05.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+    int a;
+    cin>>a;
+    int ans=a*3;
+    cout<<ans;
+}
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    solve();
+	    cout<<endl;
+	}
+	return 0;
+}
